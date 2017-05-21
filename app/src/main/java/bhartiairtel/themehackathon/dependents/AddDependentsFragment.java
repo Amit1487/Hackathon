b8ec0259554b3cbda4df5ef9c4b1ed02bacc5aa8
B0089798 <amit2.dubey@airtel.com>
2017-05-21 08:07:04 +0530

    no message

--- app/src/main/java/bhartiairtel/themehackathon/dependents/AddDependentsFragment.java
+++ app/src/main/java/bhartiairtel/themehackathon/dependents/AddDependentsFragment.java
@@ -44,9 +44,6 @@ public class AddDependentsFragment extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AddDependents.
      */
     // TODO: Rename and change types and number of parameters
